f5e87dca93d13e69f5bc091a43f3dc285619187d
Christian <csaidjm@gmail.com>
2025-11-27 10:00:26 +0100

    Corregir nombre de archivo y añadir mi nombre como autor

---
+++ src/ConversorUnidades.java
@@ -0,0 +1,18 @@
+public class ConversorUnidades {
+    
+    /**
+     * Convierte grados Celsius a Fahrenheit
+     * @param celsius Temperatura en grados Celsius
+     * @return Temperatura en grados Fahrenheit
+     */
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+    
+    // Método main para pruebas
+    public static void main(String[] args) {
+        double celsius = 25.0;
+        double fahrenheit = celsiusToFahrenheit(celsius);
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+    }
+}
